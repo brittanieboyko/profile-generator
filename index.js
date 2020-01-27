f0e868d73f7e2e767dd6289bde88984b2f6dc8dd
brittanieb <brittanie.boyko@gmail.com>
2020-01-26 23:30:03 -0500

    created question prompt array and log answers

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const questions = [
-  
+const inquirer = require("inquirer");
+const questions = [ 
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?"
+    },
+    {
+      type: "list",
+      name: "color",
+      message: "What's your favorite color?",
+      choices: ["blue","green","red","pink"]
+    }
 ];
+
+function promptUser() {
+    return inquirer.prompt(questions);
+}
 
 function writeToFile(fileName, data) {
  
 }
 
 function init() {
+    promptUser()
+        .then(function(answers) {
+            console.log(answers);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
 }
 
 init();
