035dd56195910f9a812d2b8d0ccde7843090f1fd
brittanieb <brittanie.boyko@gmail.com>
2020-01-28 23:12:08 -0500

    refactor to use promises

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const gen = require("./generateHTML.js");
 const axios = require("axios");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 const questions = [ 
     {
@@ -18,40 +21,32 @@ const questions = [
 ];
 
 function promptUser() {
-    return inquirer.prompt(questions);
+    return inquirer.prompt(questions)
 }
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("success");
-    });
-
-}
-
-function getGitHubData(data) {
-    axios
-    .get(`https://api.github.com/search/users?q=${data.username}`)
-
-    .then(function(res) {
-        console.log(res.data);
-    })
+function getGitHubData(response) {
+    const queryUrl = `https://api.github.com/search/users?q=${response.username}`;
+    console.log(color);
+    axios.get(queryUrl)
+    .then(function(result) {
+        console.log("hey this worked");
+     })
+     .catch(err => {
+         throw(err);
+     })
 }
 
 function init() {
+
     promptUser()
-        .then(function(answers) {
-            const html = gen.generateHTML(answers);
-            
-            getGitHubData(answers);
-            writeToFile("hi.html", html)
-            
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
+    .then(function(answers) {
+        const html = gen.generateHTML(answers);
+        getGitHubData(answers)
+        return  writeFileAsync("okay.html", html);
+    })
+    .catch(err => {
+        throw(err);
+    });
 }
 
 init();
